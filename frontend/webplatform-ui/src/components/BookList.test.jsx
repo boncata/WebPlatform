d7402582a686252570b4ac936da354a4a016c10e
Martin Bonev <martin.s.bonev@gmail.com>
2026-05-07 22:24:25 +0200

    Set up fronted unit test
    Set up unit test infrastructure for the fronted, using vitest.
    Initially test the BookList function.

---
+++ frontend/webplatform-ui/src/components/BookList.test.jsx
@@ -0,0 +1,34 @@
+import { describe, test, expect } from "vitest";
+
+import { render, screen } from "@testing-library/react";
+
+import BookList from "./BookList";
+
+// "describe" groups related tests, e.g. a test suite.
+describe("BookList", () => {
+  test("renders books correctly", () => {
+    const books = [
+      {
+        id: 1,
+        title: "Clean Code",
+        author: "Robert C. Martin",
+        price: 30
+      },
+      {
+        id: 2,
+        title: "Refactoring",
+        author: "Martin Fowler",
+        price: 40
+      }
+    ];
+
+    // Render component into test DOM (not real)
+    render(<BookList books={books} />);
+
+    expect(screen.getByText("Clean Code — Robert C. Martin (€30)"))
+      .toBeInTheDocument();
+
+    expect(screen.getByText("Refactoring — Martin Fowler (€40)"))
+      .toBeInTheDocument();
+  });
+});
